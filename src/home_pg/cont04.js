66f39897cc3551df3498c474bf9f15a65b4fcfff
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2024-08-05 19:30:03 +0530

    polls section logo updated

--- src/home_pg/cont04.js
+++ src/home_pg/cont04.js
@@ -15,7 +15,10 @@ function Cont04() {
 
   return (
     <section className="cont04" id="polls">
-      <div className="title"><p>Polls</p><svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 24 24" id="poll"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"></path></svg></div>
+      <div className="title"><p>Polls</p> <svg xmlns="http://www.w3.org/2000/svg" fill="none" height={40} width={40} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+      </svg>
+      </div>
 
       <Polls polls={poll} />
       {/* <video autoPlay loop muted playsInline>
